e8b35effe5fcda838eca4acdf46c1860419ff141
Gabrieldevs77 <petollolmel@gmail.com>
2020-06-02 11:20:08 -0300

    projeto alterar

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace aula_8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+             Console.WriteLine("Hello World!");
         }
     }
 }
